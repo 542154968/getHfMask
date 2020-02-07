6010f98d97c040bb1c20127810f413183a8ad966
542154968 <542154968@qq.com>
2020-02-07 19:26:20 +0800

    feat: 个人信息描述调整

--- userData.js
+++ userData.js
@@ -1,15 +1,15 @@
 module.exports = {
     // 姓名
-    name: "王xx",
+    name: "王**",
     // 身份证
-    cardNo: "341282*******0192",
+    cardNo: "341*******",
     // 手机号
-    phone: "130****8170",
+    phone: "130******",
     // 口罩数量 固定5 切勿更改
     reservationNumber: "5",
-    // 领取口罩的药店名称 该信息可查阅 common/pharmacy.json 找到您要找的药店的数据，在此输入name值即可
+    // 领取口罩的药店名称 该信息可查阅  README.md -> 使用帮助 -> 第三条
     pharmacyName: "国胜大药房坝下路店",
-    // 领取口罩的药店编号ID  该信息可查阅 common/pharmacy.json 找到您要找的药店的数据，在此输入code值即可
+    // 领取口罩的药店编号ID 该信息可查阅  README.md -> 使用帮助 -> 第三条
     pharmacyCode: "11183",
     // 通过上次请求获取到的有效的口罩信息 数量和时间点 请勿填写，自动获取生成
     pharmacyPhase: "",
