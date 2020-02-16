98911be1a122e754871427ae48a2862b7fa45ce5
542154968 <542154968@qq.com>
2020-02-16 12:40:14 +0800

    feat: 0216号，请求药房信息的接口改动

--- getPharmacy.js
+++ getPharmacy.js
@@ -1,4 +1,5 @@
 // 在此输入您想查询的药房名称，即可获取信息。 ！！！
+// 那个查询药店的时候根据药店名字或者街道来查。只能输入一个关键字。
 var pharmacyName = '坝下路'
 
 // 加密方法
@@ -6,18 +7,20 @@ var {
     isvData
 } = require("./common/isv");
 // 请求方法
-var request = require("./common/request");
+var request = require("./common/requestForm");
 // 公共配置
 var config = require("./config")
 
-var isvUrl = isvData(
-    config.urlFirst + "/mask/pharmacy-search?pharmacyName=" + encodeURI(pharmacyName)
-)
+var isvUrl = config.urlFirst + "/mask/pharmacy-search"
 
 request(
     "POST",
-    isvUrl,
-    null,
+    isvUrl, {
+        pharmacyName: pharmacyName,
+        areaId: '',
+        pageNum: 1,
+        pageSize: 100
+    },
     function (data) {
         console.log(data)
         console.log('当前药房信息已查询完毕')
